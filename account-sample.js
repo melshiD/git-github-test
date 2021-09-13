da5783e7ada72d3e401269b1f0917b4f6fc65ac7
David Melsheimer <dave.melshman@gmail.com>
2021-09-13 18:42:23 -0400

    gitignore setup and account js api key

---
+++ account-sample.js
@@ -0,0 +1,2 @@
+const API-KEY = 'Enter you API key here in accounts.js';
+export default API_KEY;
---
+++ account.js
@@ -0,0 +1,2 @@
+const API-KEY = '3bd6a84ab26b6166d84f4ed2a01a0836031653ce';
+export default API_KEY;
