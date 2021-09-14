2bc666df39c649666115d91a6efb0bfd3fbf5609
David Melsheimer <dave.melshman@gmail.com>
2021-09-14 08:31:36 -0400

    commiting new account.js file.  Maybe i need to use the sample account file instead

--- account.js
+++ account.js
@@ -1,4 +1,6 @@
-const API-KEY = '3bd6a84ab26b6166d84f4ed2a01a0836031653ce';
+console.time('label');
+const API_KEY = '3bd6a84ab26b6166d84f4ed2a01a0836031653ce';
 export default API_KEY;
+console.timeEnd('label');
 
-//change made for training purposes
+//here we see a stash after a save 
