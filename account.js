3d4f9b203023d14eb41377dad3c37135aeb08696
David Melsheimer <dave.melshman@gmail.com>
2021-09-14 08:58:47 -0400

    adding links to about and index pages

--- account.js
+++ account.js
@@ -3,4 +3,16 @@ const API_KEY = '3bd6a84ab26b6166d84f4ed2a01a0836031653ce';
 export default API_KEY;
 console.timeEnd('label');
 
+<<<<<<< Updated upstream
 //here we see a stash after a save 
+=======
+<<<<<<< Updated upstream
+//change made for training purposes
+=======
+//here we see a stash after a save 
+
+const myFunction = () => {
+    console.log(time);
+}
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
